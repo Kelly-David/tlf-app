a94aa4b1efb8cfc936776879a9ec23a09b419b52
kelly-david <davidkelly.tlf@gmail.com>
2018-05-22 16:22:08 +0100

    New component - horse list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HorseListComponent } from './horses/horse-list/horse-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HorseListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/horses/horse-list/horse-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horse-list',
+  templateUrl: './horse-list.component.html',
+  styleUrls: ['./horse-list.component.css']
+})
+export class HorseListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
