d8c603ab3ba74f5928dfc3b7589bdec8fde548e0
kelly-david <davidkelly.tlf@gmail.com>
2018-05-22 19:20:07 +0100

    horses route

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HorseListComponent } from '../horses/horse-list/horse-list.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full',  redirectTo: 'horses' },
+  { path: 'horses', component: HorseListComponent},
+  { path: '**', redirectTo: 'horses'}, // Fallback route (TODO page not found component)
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
-  declarations: []
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
