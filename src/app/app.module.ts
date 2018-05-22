80ca737c1643d51be4295bfb14a5175195dd41d3
kelly-david <davidkelly.tlf@gmail.com>
2018-05-22 19:17:00 +0100

    Added app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HorseListComponent } from './horses/horse-list/horse-list.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { HorseListComponent } from './horses/horse-list/horse-list.component';
     HorseListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
